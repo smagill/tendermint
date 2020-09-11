c7921ecba6e29dffb1c2486eed871c6511e33517
Erik Grinaker <erik@interchain.berlin>
2020-09-11 15:25:02 +0200

    restructured cli and added cleanup command

--- test/e2e/runner/main.go
+++ test/e2e/runner/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -10,12 +11,27 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 )
 
-var (
-	logger  = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-	rootCmd = &cobra.Command{
+var logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+func main() {
+	NewCLI().Run()
+}
+
+// CLI is the Cobra-based command-line interface.
+type CLI struct {
+	root    *cobra.Command
+	testnet *Testnet
+	dir     string
+	binary  string
+}
+
+// NewCLI sets up the CLI.
+func NewCLI() *CLI {
+	cli := &CLI{}
+	cli.root = &cobra.Command{
 		Use:   "runner",
 		Short: "End-to-end test runner",
-		RunE: func(cmd *cobra.Command, args []string) error {
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			file, err := cmd.Flags().GetString("file")
 			if err != nil {
 				return err
@@ -40,26 +56,77 @@ var (
 			if err != nil {
 				return err
 			}
-			err = Setup(testnet, dir, binary)
-			if err != nil {
+
+			cli.testnet = testnet
+			cli.dir = dir
+			cli.binary = binary
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := cli.Setup(); err != nil {
 				return err
 			}
-			logger.Info(fmt.Sprintf("Generated testnet files in %q", dir))
+			if err := cli.Cleanup(); err != nil {
+				return err
+			}
 			return nil
 		},
 	}
-)
 
-func init() {
-	rootCmd.Flags().StringP("file", "f", "", "Testnet TOML manifest")
-	_ = rootCmd.MarkFlagRequired("file")
-	rootCmd.Flags().StringP("dir", "d", "", "Directory to use for testnet data (defaults to manifest dir)")
-	rootCmd.Flags().StringP("binary", "b", "../../build/tendermint", "Tendermint Linux binary to hardlink into containers")
+	cli.root.PersistentFlags().StringP("file", "f", "", "Testnet TOML manifest")
+	_ = cli.root.MarkPersistentFlagRequired("file")
+	cli.root.PersistentFlags().StringP("dir", "d", "",
+		"Directory to use for testnet data (defaults to manifest dir)")
+	cli.root.PersistentFlags().StringP("binary", "b", "../../build/tendermint",
+		"Tendermint binary to hardlink into containers")
+
+	cli.root.AddCommand(&cobra.Command{
+		Use:   "setup",
+		Short: "Generates the testnet directory and configuration",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cli.Setup()
+		},
+	})
+
+	cli.root.AddCommand(&cobra.Command{
+		Use:   "cleanup",
+		Short: "Removes the testnet directory",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cli.Cleanup()
+		},
+	})
+
+	return cli
 }
 
-func main() {
-	if err := rootCmd.Execute(); err != nil {
+// Run runs the CLI.
+func (cli *CLI) Run() {
+	if err := cli.root.Execute(); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+// Setup generates the testnet configuration.
+func (cli *CLI) Setup() error {
+	logger.Info(fmt.Sprintf("Generating testnet files in %q", cli.dir))
+	err := Setup(cli.testnet, cli.dir, cli.binary)
+	if err != nil {
+		return err
+	}
+	logger.Info(fmt.Sprintf("Generated testnet files in %q", cli.dir))
+	return nil
+}
+
+// Cleanup removes the testnet directory.
+func (cli *CLI) Cleanup() error {
+	if cli.dir == "" {
+		return errors.New("no directory set")
+	}
+	logger.Info(fmt.Sprintf("Removing testnet directory %q", cli.dir))
+	err := os.RemoveAll(cli.dir)
+	if err != nil {
+		return err
+	}
+	return nil
 }
